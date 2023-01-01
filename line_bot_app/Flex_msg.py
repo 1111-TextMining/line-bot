5065377072a3a1079190fe29c6f8024336625e54
cydaf <53554367+cydaf@users.noreply.github.com>
2023-01-01 21:15:43 +0800

    modify comment

--- line_bot_app/Flex_msg.py
+++ line_bot_app/Flex_msg.py
@@ -11,12 +11,12 @@ def house(result_text):
     # datas = House.objects.filter(uid=uid)
     for i in range(len(result_text)):
         house_name = result_text[i][0]  # 第0個位置表標題
-        section = result_text[i][1]  # 第0個位置表標題
-        price = result_text[i][2]  # 第1個位置表價格
-        layout = result_text[i][3]  # 第2個位置表幾房幾廳
-        area = result_text[i][4]  # 第3個位置表坪數
-        url = result_text[i][5]  # 第4個位置表591url
-        pic = result_text[i][6]  # 第5個位置表圖片
+        section = result_text[i][1]  # 第1個位置表低區
+        price = result_text[i][2]  # 第2個位置表價格
+        layout = result_text[i][3]  # 第3個位置表幾房幾廳
+        area = result_text[i][4]  # 第4個位置表坪數
+        url = result_text[i][5]  # 第5個位置表591url
+        pic = result_text[i][6]  # 第6個位置表圖片
         print(pic, url, house_name)
         bubble = {
             "type": "bubble",
